2b84ee48c9156a83344a8ced9fb79acbf4713472
henrywu <henrw@umich.edu>
2022-04-26 01:02:05 -0400

    Initial Commit

---
+++ My Shortcuts/My_ShortcutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_ShortcutsApp.swift
+//  My Shortcuts
+//
+//  Created by Henry Wu on 4/26/22.
+//
+
+import SwiftUI
+
+@main
+struct My_ShortcutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
